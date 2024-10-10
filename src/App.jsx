105be1217a18bd981da844ba6ddefabf0c2cc491
Suhani657 <suhani190505@gmail.com>
2024-10-10 22:05:55 +0530

    text-analysis

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import './App.css';
+import axios from "axios";
+
+function App() {
+  const [question, setQuestion] = useState("");
+  const [answer,setAnswer] = useState("");
+
+  async function generateAnswer() {
+    setAnswer("loading..");
+    const response = await axios({
+      url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyDmyLClj9UML9mb5HJpFAOrLucK7pM7skM",
+      method: "post",
+      data: {
+        contents: [
+          { "parts": [{ "text": question }] },
+        ],
+      },
+    });
+    setAnswer(response['data']['candidates'][0]['content']['parts'][0]["text"]);
+  }
+  return (
+    <>
+      <div className='w-full'></div>
+      <h1 className='bg-red-300'>Chat AI</h1>
+      <textarea 
+      className='border rounded w-full'
+      value={question} onChange={(e) => setQuestion(e.target.value)} cols="30" rows="10"
+      placeholder='Ask anything to me'  
+      ></textarea>
+      <button onClick={generateAnswer}>Generate Answer</button>
+
+      <pre>{answer}</pre>
+    </>
+  )
+}
+
+export default App
